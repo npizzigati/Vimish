f94bcc36b48c57fecae870e59529576b939860c7
Nick Pizzigati <npizzigati@gmail.com>
2021-08-12 13:41:07 -0500

    Implement first KeySequence and Actions tests

---
+++ src/test/java/org/omegat/plugins/vimish/ActionsTest.java
@@ -0,0 +1,44 @@
+package org.omegat.plugins.vimish;
+
+import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
+import static org.assertj.core.api.Assertions.*;
+
+import org.omegat.gui.editor.EditorController;
+import org.omegat.gui.editor.IEditor.CaretPosition;
+
+class ActionsTest {
+  EditorController mockEditor;
+  Actions actions;
+
+  @BeforeEach
+  void setUp() {
+    mockEditor = mock(EditorController.class);
+    actions = new Actions(mockEditor);
+  }
+
+  @Test
+  void testNormalForwardChar() {
+    when(mockEditor.getCurrentPositionInEntryTranslation()).thenReturn(5);
+    when(mockEditor.getCurrentTranslation()).thenReturn("This is a test");
+
+    Actions actionsSpy = spy(actions); 
+    actionsSpy.normalForwardChar("", 1);
+
+    verify(actionsSpy).setCaretIndex(6);
+  }
+
+  @Test
+  void testNormalForwardCharNearEndOfSegment() {
+    // Must not set character index beyond end of segment
+    when(mockEditor.getCurrentPositionInEntryTranslation()).thenReturn(10);
+    when(mockEditor.getCurrentTranslation()).thenReturn("This is a test");
+
+    Actions actionsSpy = spy(actions); 
+    actionsSpy.normalForwardChar("", 6);
+
+    verify(actionsSpy).setCaretIndex(14);
+  }
+}
