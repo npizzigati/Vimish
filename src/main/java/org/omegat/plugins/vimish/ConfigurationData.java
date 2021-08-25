b7433032e03718fe9ba635a94da9204ca144f977
Nick Pizzigati <npizzigati@gmail.com>
2021-08-24 19:08:23 -0500

    Add annotation to prevent unmarshalling error on unknown properties

--- src/main/java/org/omegat/plugins/vimish/ConfigurationData.java
+++ src/main/java/org/omegat/plugins/vimish/ConfigurationData.java
@@ -1,9 +1,15 @@
 package org.omegat.plugins.vimish;
 
 import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+// This annotation will prevent the Jackson unmarshaller
+// from throwing an exception if we add fields in the future
+// that are not contained in the user's configuration file
+@JsonIgnoreProperties(ignoreUnknown = true)
 class ConfigurationData {
   public boolean moveCursorBack;
   public Map<String, String> keyMappings;
+  public Map<String, String> keyChords;
   public Map<String, String> abbreviations;
 }
